392227f209161b91f3b8c757457233c5cccb7417
Fardeau <fardeau@Host-003.lan>
2026-06-18 23:56:33 +0200

    Rajout du début du Lexer / Parse -> Récupération des tokens bruts, commencement de rajout des types et du sens

--- utils/utils.cpp
+++ utils/utils.cpp
@@ -2,12 +2,15 @@
 #include <string>
 
 
+// RESETS + ENDL THE OSTREAM
 std::ostream& endofline(std::ostream& os) {
     return os << RESET << std::endl;
 }
 
+// PRINTS STRING
 void display(std::string print) { std::cout << print << endofline; }
 
+// CHECKS IF CURRENT KEY BELONGS TO KEYS_LIST
 bool    isValidKey(const std::string &key, const std::string keys_list[], const size_t size) {
     for (size_t i = 0; i < size; i++) {
         if (keys_list[i] == key)
